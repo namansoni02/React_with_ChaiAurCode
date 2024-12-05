b93e634313f9445db3f3ec9718dffd8bbc4a579f
me <namansoniop@gmail.com>
2024-12-05 16:17:42 +0530

    Completed 05passwordGenerator

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -4,11 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("Yellow");
 
   return (
     <>
-      <h1 className="bg-purple-500">hi </h1>
+      <div className="w-full h-screen" style={{ backgroundColor: color }}>
+        <div className='flex justify-center mx-4 my-2 absolute bottom-20 w-full'>
+            <button className='mx-3 p-2 bg-red-500 rounded-xl' onClick={() => { setColor("red") }}>Red</button>
+            <button className='mx-3 p-2 bg-blue-500 rounded-xl' onClick={() => { setColor("blue") }}>Blue</button>
+            <button className='mx-3 p-2 bg-green-500 rounded-xl' onClick={() => { setColor("green") }}>Green</button>
+        </div>
+      </div>
     </>
   )
 }
